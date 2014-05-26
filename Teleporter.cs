44eb164e3188ac4e62c213772704515dfe918dfd
Danubian <stjohnzack@yahoo.com>
2014-05-25 20:17:09 -0700

    Updated Teleporter

--- Teleporter.cs
+++ Teleporter.cs
@@ -2,28 +2,20 @@
 using System.Collections;
 
 public class Teleporter : MonoBehaviour {
-    float playerHeight;
+	public GameObject TeleportTo;
+
 	// Use this for initialization
 	void Start () {
-        playerHeight = 1.32f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
 	}
 
     void OnTriggerEnter (Collider other) {
-        print(gameObject.name);
         Vector3 displacement = other.transform.position - this.transform.position;
-        if(gameObject.name.Equals("Teleporter_A")){
-            other.transform.position = GameObject.Find("End_A").transform.position;
-            other.transform.position += displacement;
-        }
-        else if (gameObject.name.Equals("Teleporter_B"))
-        {
-            other.transform.position = GameObject.Find("End_B").transform.position;
-            other.transform.position += displacement;
-        }
+
+		other.transform.position = TeleportTo.transform.position;
+		other.transform.position += displacement;
 	}
 }
