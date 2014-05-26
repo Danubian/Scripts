f60cbc2b35066bdf570dd7626ab7e79d3987f44f
Danubian <stjohnzack@yahoo.com>
2014-05-25 19:35:52 -0700

    Update 2

---
+++ FinnickyLight.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FinnickyLight : MonoBehaviour {
+
+    float intensity;
+	// Use this for initialization
+	void Start () {
+        intensity = this.light.intensity;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter () {
+        this.light.intensity = 0;
+
+        Invoke("TurnBack", Random.Range(5.0f, 20.0f));
+	}
+
+    void TurnBack()
+    {
+        this.light.intensity = intensity;
+    }
+}
---
+++ Teleporter.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Teleporter : MonoBehaviour {
+    float playerHeight;
+	// Use this for initialization
+	void Start () {
+        playerHeight = 1.32f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter (Collider other) {
+        print(gameObject.name);
+        Vector3 displacement = other.transform.position - this.transform.position;
+        if(gameObject.name.Equals("Teleporter_A")){
+            other.transform.position = GameObject.Find("End_A").transform.position;
+            other.transform.position += displacement;
+        }
+        else if (gameObject.name.Equals("Teleporter_B"))
+        {
+            other.transform.position = GameObject.Find("End_B").transform.position;
+            other.transform.position += displacement;
+        }
+	}
+}
